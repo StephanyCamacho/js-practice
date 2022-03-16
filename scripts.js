540c3aa5dd7bbb5967d3dcf164c2e2ff1a241a03
StephanyCamacho <stephany_c19@yahoo.com>
2022-03-15 23:38:47 -0600

    practice

--- scripts.js
+++ scripts.js
@@ -1,7 +1,53 @@
-// This is a single line comment
 
-//Hi there,
-//How are you?
+// ------------ DAILY CHALLENGE ------------
+// Prompt the user for two different numbers
+// Convert the values to integers
+// Store the two integers as variables
+// Compare the two numbers
+// Alert the larger number
 
-console.log("Stephany Camacho-Lara");
-console.log("My favorite food is sushi.");
+// let num1 = prompt ("Please type a number");
+// let num2 = prompt ("Please type another number");
+
+// let newNum1 = parseInt(num1)
+// let newNum2 = parseInt(num2)
+
+// if (newNum1 < newNum2){
+//   alert (`Number ${newNum2} is bigger than ${newNum1}`)
+// }
+// else {
+//   alert (`Number ${newNum1} is bigger than ${newNum2}`)
+// }
+
+
+
+// ------------ TAKE HOME CHALLENGE ------------
+// Add an edge case to your daily challenge code
+// Check if the user input anything other than a number
+// If so, prompt the user again to input a number
+
+// let num1 = prompt ("Please type a number");
+
+// if (isNaN(num1)){
+//   alert("Enter a valid number")
+//   prompt ("Please type a valid number")
+// }
+
+// let num2 = prompt ("Please type another number");
+
+// if (isNaN(num2)){
+//   alert("Enter a valid number")
+//   prompt ("Please type a valid number")
+// }
+
+
+// let newNum1 = parseInt(num1)
+// let newNum2 = parseInt(num2)
+
+
+// if (newNum1 < newNum2){
+//   alert (`Number ${newNum2} is bigger than ${newNum1}`)
+// }
+// else {
+//   alert (`Number ${newNum1} is bigger than ${newNum2}`)
+// }
